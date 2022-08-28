2458067f0151516954fee98aa67487f0974bd7be
Дмитрий Анфимов <danfimov@yandex-team.ru>
2022-08-29 00:40:29 +0300

    Try to add tests to workflow v11

--- delivery_hub/db/connection.py
+++ delivery_hub/db/connection.py
@@ -6,7 +6,7 @@ from delivery_hub.config import get_settings
 
 settings = get_settings()
 redis: Redis = get_redis_connection(
-    host=settings.APP_HOST,
+    host=settings.REDIS_HOST,
     port=settings.REDIS_PORT,
     password=settings.REDIS_PASSWORD,
     decode_responses=True,
